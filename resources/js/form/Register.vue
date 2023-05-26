7a99089ba86da40a91b7a52eb8d03968b7e11d4a
Marcel Stadelmann <m@marceli.to>
2023-05-26 17:01:23 +0200

    wip

--- resources/js/form/Register.vue
+++ resources/js/form/Register.vue
@@ -12,7 +12,7 @@
       <form v-if="isFetched">
         <form-grid>
           <form-group class="col-span-6">
-            <select v-model="form.event" class="w-full border-0 px-10 lg:py-15 w-full font-black bg-[center_left_.4rem] bg-[url('/img/chevron-down.svg')] bg-[length:20px_10px] pl-35 text-md ring-0 focus:ring-0 outline-none">
+            <select v-model="form.event" class="w-full border-0 px-10 lg:py-15 w-full font-black bg-[center_left_.4rem] bg-[url('/img/chevron-down.svg')] bg-[length:20px_10px] pl-35 ring-0 focus:ring-0 outline-none">
               <option v-for="event in events" :key="event.id" :value="event.title">
                 {{ event.title }}
               </option>
